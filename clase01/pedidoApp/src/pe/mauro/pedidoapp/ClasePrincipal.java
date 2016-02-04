84c868bf9a57293f92dc50bd94dfaae18b2e3e7d
Mauro Fernanadez <mauro_9_2@hotmail.com>
2016-02-04 08:49:11 -0500

    clase 1 java

--- clase01/pedidoApp/src/pe/mauro/pedidoapp/ClasePrincipal.java
+++ clase01/pedidoApp/src/pe/mauro/pedidoapp/ClasePrincipal.java
@@ -13,7 +13,7 @@ public class ClasePrincipal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Holaaaa");
+        System.out.println("Hola");
         PedidoView.main(null);
     }
     
